b9996999d139e4978b05deb08dd23fa9455108c7
Nicholas Sellek <nick@seansellek.com>
2016-02-14 11:28:35 -0500

    allows creation of account with password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+  
   has_many :user_would_you_rathers
   has_many :would_you_rathers, through: :user_would_you_rathers
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes["password"] }
+  validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
+  validates :name, presence: true
+
+  validates :email, uniqueness: true
 end
